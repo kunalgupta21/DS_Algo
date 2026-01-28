eb133de5e5ebdabed33f6fa9fa58bacacc896b2b
Kunal Gupta <167274095+kunalgupta21@users.noreply.github.com>
2026-01-28 12:18:21 +0530

    Time: 10 ms (7.17%), Space: 55.1 MB (5.07%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -7,7 +7,7 @@ class Solution {
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(vis[i][j]==0 && grid[i][j]=='1'){
-                    dfs(i,j,grid,vis);
+                    bfs(i,j,grid,vis);
                     count++;
                 }
             }
@@ -30,5 +30,36 @@ class Solution {
             
         
         
+    }
+
+    private void bfs(int i,int j,char[][] grid,int[][] vis){
+        
+        vis[i][j]=1;
+        Queue<int[]> que=new LinkedList<>();
+        
+        que.add(new int[]{i,j});
+        
+        int n=grid.length;
+        int m=grid[0].length;
+        
+        while(!que.isEmpty()){
+            
+               int[] temp=que.poll();
+                int st=temp[0];
+                int end=temp[1];
+                int[] delrow={-1,0,0,1};
+                int[] delcol={0,-1,1,0};
+                for(int k=0;k<delrow.length;k++){
+                    int row=st+delrow[k];
+                    int col=end+delcol[k];
+                    if(row>=0 && row<n && col>=0 && col<m && vis[row][col]==0 && grid[row][col]=='1'){
+                            vis[row][col]=1;
+                            
+                            que.add(new int[]{row,col});
+                            
+                        }
+                }
+         }
+            
     }
 }
