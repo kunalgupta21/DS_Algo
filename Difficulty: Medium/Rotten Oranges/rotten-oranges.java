6f3097eb382dc55d8d68ed4b128ede4ab15b00f0
Kunal Gupta <167274095+kunalgupta21@users.noreply.github.com>
2026-01-30 06:18:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotten Oranges/rotten-oranges.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int orangesRot(int[][] mat) {
+        // code here
+        int cntfresh=0;
+        int n=mat.length;
+        int m=mat[0].length;
+        int[][] vis=new int[n][m];
+        Queue<int[]> que=new ArrayDeque<>();
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==2){
+                    vis[i][j]=2;
+                    que.add(new int[]{i,j,0});
+                }
+                if(mat[i][j]==1) cntfresh++;
+            }
+        }
+        int[] drow={-1,0,0,1};
+        int[] dcol={0,-1,1,0};
+        int cnt=0;
+        int mxt=0;
+        while(!que.isEmpty()){
+            int[] temp=que.poll();
+            int st=temp[0],end=temp[1],t=temp[2];
+            mxt=Math.max(mxt,t);
+            for(int k=0;k<4;k++){
+                int row=st+drow[k];
+                int col=end+dcol[k];
+                if(row>=0 && row<n && col>=0 && col<m && vis[row][col]!=2 && mat[row][col]==1){
+                    vis[row][col]=2;
+                    que.add(new int[]{row,col,t+1});
+                    cnt++;
+                }
+            }
+            
+        }
+        if(cnt!=cntfresh) return -1;
+        return mxt;
+        
+    }
+}
