9459a078d49a1feacd9b3c5030677e6fa207f410
Kunal Gupta <167274095+kunalgupta21@users.noreply.github.com>
2026-02-13 03:13:39 +0530

    Time: 1 ms (100%), Space: 43.5 MB (80%) - LeetHub

--- 3258-count-substrings-that-satisfy-k-constraint-i/3258-count-substrings-that-satisfy-k-constraint-i.java
+++ 3258-count-substrings-that-satisfy-k-constraint-i/3258-count-substrings-that-satisfy-k-constraint-i.java
@@ -1,29 +1,25 @@
 class Solution {
-    public int countKConstraintSubstrings(String a, int k) {
-    //    ArrayList<String> arr=new ArrayList<>();
-    //    substring(a,"",arr,0);
-       int count=0;
-        for(int i=0;i<a.length();i++){
-            for(int j=i;j<a.length();j++){
-                 if(isValid(a.substring(i,j+1),k)){
-                    count++;
-                }
-            }
-        }
-       
-       
-       return count;
-    }
-   
-    public boolean isValid(String str,int k){
+    public int countKConstraintSubstrings(String s, int k) {
+        int n=s.length();
+        int i=0,j=0;
         int c1=0,c0=0;
-        for(char ch:str.toCharArray()){
+        int count=0;
+        while(j<n){
+            char ch=s.charAt(j);
             if(ch=='1') c1++;
             else c0++;
+
+            while(c1>k && c0>k){
+                if(s.charAt(i)=='0'){
+                    c0--;
+                }else{
+                    c1--;
+                }
+                i++;
+            }
+            count+=j-i+1;
+            j++;
         }
-        if(c1<=k || c0<=k) return true;
-        return false;
-
+        return count;
     }
-
 }
